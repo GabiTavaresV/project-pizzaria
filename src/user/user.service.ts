e09b689e4c142181098d13f6afc92ddfa2853896
s40177443898 <gabriela.virissimo@dasa.com.br>
2022-11-07 17:26:03 -0300

    ✅ feat: add UserService

---
+++ src/user/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  createUser(data: any) {
+    return user;
+  }
+}
+
+
+/*
+Id (BD - Back-end)
+Nome Completo
+Endereço (Rua, Número, Cep, Bairro, Cidade e Estado)
+E-mail
+Telefone
+Senha
+
+*/
